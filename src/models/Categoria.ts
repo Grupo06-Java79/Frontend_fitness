b62c049603ecd15f3b0efdb4d63e5437703f3499
Guilherme Kaludin <guikaludin@outlook.com>
2025-03-05 09:09:40 -0300

    flip login e cadastro

--- src/models/Categoria.ts
+++ src/models/Categoria.ts
@@ -1,5 +1,8 @@
+import Exercicio from "./Exercicio";
+
 export default interface Categoria {
     id: number;
     tipo: string;
     grupo: string;
+    exercicio?: Exercicio | null;
 }
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,94 +4,73 @@ import { AuthContext } from '../../contexts/AuthContext';
 import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import UsuarioLogin from '../../models/UsuarioLogin';
 import { RotatingLines } from 'react-loader-spinner';
+import Cadastrar from '../cadastrar/Cadastrar';
 
 function Login() {
-
     const navigate = useNavigate();
+    const { usuario, handleLogin, isLoading } = useContext(AuthContext);
 
-    const { usuario, handleLogin, isLoading } = useContext(AuthContext)
-
-    const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
-        {} as UsuarioLogin
-    )
+    const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>({} as UsuarioLogin);
 
     useEffect(() => {
-        if (usuario.token !== "") {
-            navigate('/home')
+        if (usuario?.token) {
+            navigate('/home');
         }
-    }, [usuario])
+    }, [usuario, navigate]);
 
     function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
         setUsuarioLogin({
             ...usuarioLogin,
             [e.target.name]: e.target.value
-        })
+        });
     }
 
     function login(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault()
-        handleLogin(usuarioLogin)
+        e.preventDefault();
+        handleLogin(usuarioLogin);
     }
 
     return (
-        <>
-            <div className="grid grid-cols-1 lg:grid-cols-2 
-                    h-screen place-items-center font-bold ">
-                <form className="flex justify-center items-center flex-col w-1/2 gap-4"
-                    onSubmit={login}>
-                    <h2 className="text-slate-900 text-5xl ">Entrar</h2>
-                    <div className="flex flex-col w-full">
-                        <label htmlFor="usuario">Usuário</label>
-                        <input
-                            type="text"
-                            id="usuario"
-                            name="usuario"
-                            placeholder="Usuario"
-                            className="border-2 border-slate-700 rounded p-2"
-                            value={usuarioLogin.usuario}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                        />
+        <div className="wrapper flex items-center justify-center py-56">
+            <div className="card-switch">
+                <label className="switch">
+                    <input className="toggle" type="checkbox" />
+                    <span className="slider"></span>
+                    <span className="card-side"></span>
+                    <div className="flip-card__inner">
+                        <div className="flip-card__front">
+                            <div className="title">Login</div>
+                            <form onSubmit={login} className="flip-card__form flex flex-col items-center">
+                                <input 
+                                    type="text" 
+                                    placeholder="Usuário" 
+                                    name="usuario" 
+                                    className="flip-card__input"
+                                    value={usuarioLogin.usuario || ''}
+                                    onChange={atualizarEstado} 
+                                />
+                                <input 
+                                    type="password" 
+                                    placeholder="Senha" 
+                                    name="senha" 
+                                    className="flip-card__input"
+                                    value={usuarioLogin.senha || ''}
+                                    onChange={atualizarEstado} 
+                                />
+                                <button className="flip-card__btn" type="submit" disabled={isLoading}>
+                                    {isLoading ? <RotatingLines strokeColor="#75BA23" strokeWidth="5" animationDuration="0.75" width="24" visible={true} /> : "Entrar"}
+                                </button>
+                            </form>
+                        </div>
+                        <div className="flip-card__back flex flex-col items-center">
+                            
+                            <div className="title">Cadastrar</div>
+                            <Cadastrar />
+                        </div>
                     </div>
-                    <div className="flex flex-col w-full">
-                        <label htmlFor="senha">Senha</label>
-                        <input
-                            type="password"
-                            id="senha"
-                            name="senha"
-                            placeholder="Senha"
-                            className="border-2 border-slate-700 rounded p-2"
-                            value={usuarioLogin.senha}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                        />
-                    </div>
-                    <button
-                        type='submit'
-                        className="rounded bg-indigo-400 flex justify-center
-                                   hover:bg-indigo-900 text-white w-1/2 py-2">
-                                    
-                        {isLoading ? <RotatingLines
-                            strokeColor="white"
-                            strokeWidth="5"
-                            animationDuration="0.75"
-                            width="24"
-                            visible={true}
-                        /> :
-                            <span>Entrar</span>
-                        }
-                    </button>
-
-                    <hr className="border-slate-800 w-full" />
-
-                    <p>
-                        Ainda não tem uma conta?{' '}
-                        <Link to="/cadastro" className="text-indigo-800 hover:underline">
-                            Cadastre-se
-                        </Link>
-                    </p>
-                </form>
-                <div className="fundoLogin hidden lg:block"></div>
+                </label>
             </div>
-        </>
+        </div>
     );
 }
 
