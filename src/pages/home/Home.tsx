cd1bdb375e20cbdf3d509c1571b17886d4a6db78
Lucas300 <ludaniel.sd@gmail.com>
2025-03-05 18:36:42 -0300

    Método IMC

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,41 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import Usuario from "../../models/Usuario";
+import { buscar } from "../../services/Service";
+
 
 function Home() {
+
+    //pegamos os dados do usuario logado
+    const { usuario } = useContext(AuthContext);
+    // Criamos um objeto do tipo Usuario que usamos para acessar dados não disponiveis no authcontext
+    const [cliente, setCliente] = useState<Usuario>({
+        id: 0,
+        nome: "",
+        usuario: "",
+        senha: "",
+        foto: "",
+        imc:0,
+        peso: 0,
+        altura: 0,
+        idade: 0,
+    });
+
+    // Pegamos os dados do usuario logado
+    async function buscaDados() {
+        try {
+            await buscar(`usuarios/${usuario.id}`, setCliente, { headers: { Authorization: usuario.token } })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // Pegamos os dados do usuario logado
+    useEffect(() => {
+        buscaDados()
+    }, [usuario.id])
+
+    
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
@@ -7,6 +43,7 @@ function Home() {
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
                             Seja Bem Vindo!
+                            {cliente.imc}
                         </h2>
                         <p className='text-xl'>
                             {/* Testar o novo app de treinos */}
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://backendfitness-7hpy.onrender.com/'
+    baseURL: 'http://localhost:8080/'
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
