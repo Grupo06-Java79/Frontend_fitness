261cc67b17f604e0615efd1574d9cb8d39908000
Guilherme Kaludin <guikaludin@outlook.com>
2025-03-06 19:00:25 -0300

    fotos

--- src/components/categoria/listacategorias/ListaObesidade.tsx
+++ src/components/categoria/listacategorias/ListaObesidade.tsx
@@ -14,14 +14,14 @@ function ListaObesidade() {
     const token = usuario.token;
 
     const imagens = [ 
-        { src: "/img/surf.png" },
+        { src: "/img/Surf.png" },
         { src: "/img/Simulador de Remo.png" },
         { src: "/img/Simulador de Cavalgada.png" },
         { src: "/img/Simulador de Caminhada.png" },
         { src: "/img/Rotação Vertical.png" },
         { src: "/img/Leg Press.png" },
-        { src: "/img/esqui.png" },
-        { src: "/img/alongador.png" }
+        { src: "/img/Esqui.png" },
+        { src: "/img/Alongador.png" }
     ];
 
     async function buscarExercicio() {
