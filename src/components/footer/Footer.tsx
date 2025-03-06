4b3dacdf8fd08ec56c624609c280482919ef306c
jloliveira95 <jl.oliveira95@gmail.com>
2025-03-06 12:24:03 -0300

    ajustes atualizados

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,22 +4,30 @@ function Footer() {
     let data = new Date().getFullYear();
 
     return (
-        <div className="w-full bg-black text-white py-4 flex justify-center">
-            <div className="flex items-center space-x-5">
-                <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
-                    <LinkedinLogo size={36} weight='bold' />
-                </a>
-                <a href="https://www.instagram.com/seu_usuario" target="_blank">
-                    <InstagramLogo size={36} weight='bold' />
-                </a>
-                <a href="https://www.facebook.com/seu_usuario" target="_blank">
-                    <FacebookLogo size={36} weight='bold' />
-                </a>
-                <p className="text-xl font-bold">
-                    PraçaFit | {data}
-                </p>
+        <>
+            <div className="flex">
+                <div className="flex w-100 pr-5 bg-black text-white">
+                    <div className="flex p-2">
+                        <a href="https://www.linkedin.com/in/seu_usuario" target="_blank" className="pl-5">
+                            <LinkedinLogo size={36} weight='bold'/>
+                        </a>
+                        <a href="https://www.instagram.com/seu_usuario" target="_blank" className="px-5">
+                            <InstagramLogo size={36} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/seu_usuario" target="_blank" >
+                            <FacebookLogo size={36} weight='bold' />
+                        </a>
+                        <p className='text-xl font-bold px-5'>
+                            PraçaFit
+                        </p>
+                        <p className='text-xl font-bold '>
+                            {data}
+                        </p>
+                    </div>
+                </div>
+                <div className="bg-[#CEF9A9] w-full"/>
             </div>
-        </div>
+        </>
     );
 }
 
